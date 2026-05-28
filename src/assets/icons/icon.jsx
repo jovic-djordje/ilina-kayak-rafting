a8b11fd655f3f9b09624ea1b37f2a7e7ef31c7f8
jovic-djordje <jovicdjordje21@gmail.com>
2026-05-28 17:49:26 +0200

    working on service section

--- src/assets/icons/icon.jsx
+++ src/assets/icons/icon.jsx
@@ -1,7 +1,39 @@
 import menuIcon from "./icons8-menu.svg";
+import serviceCartIcon from "./icons8-rafting-30.png";
+import serviceCartIconTwo from "./icons8-kayaking-30.png";
+import serviceCartIconThree from "./icons8-canoe-50.png";
+import serviceCartIconFour from "./icons8-party-50.png";
 
 const MenuIcon = ({ className }) => {
   return <img src={menuIcon} alt="Menu" className={className} />;
 };
 
-export { MenuIcon };
+const ServiceCartIcon = ({ className }) => {
+  return <img src={serviceCartIcon} alt="rafting icon" className={className} />;
+};
+
+const ServiceCartIconTwo = ({ className }) => {
+  return (
+    <img src={serviceCartIconTwo} alt="kayaking icon" className={className} />
+  );
+};
+
+const ServiceCartIconThree = ({ className }) => {
+  return (
+    <img src={serviceCartIconThree} alt="canoe icon" className={className} />
+  );
+};
+
+const ServiceCartIconFour = ({ className }) => {
+  return (
+    <img src={serviceCartIconFour} alt="party icon" className={className} />
+  );
+};
+
+export {
+  MenuIcon,
+  ServiceCartIcon,
+  ServiceCartIconTwo,
+  ServiceCartIconThree,
+  ServiceCartIconFour,
+};
